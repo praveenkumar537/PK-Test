7003b24a9b5b07289800ee305919beba768f7d90
Praveen Kumar Kandhagatla <praveen.kandhagatla@salesforce.com>
2018-12-27 15:02:19 +0530

    test commit 3 removed components

--- build/ftests/resource/generateManifest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#dump all class names into the manifest
-grep -il "@istest" ../src/classes/*.cls  | awk -F "/" ' { print $NF }' | awk -F "." ' {print $1} ' > ./ftests/resource/manifestClasses.txt
